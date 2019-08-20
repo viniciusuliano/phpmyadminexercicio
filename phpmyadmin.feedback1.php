60e1ae3594791a4f290bcb87151a956ca49cffbd
Vinicius Uliano <48574234+viniciusuliano@users.noreply.github.com>
2019-08-20 10:35:30 -0300

    Create phpmyadmin.feedback1.php

---
+++ phpmyadmin.feedback1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Formulário de Feedback</title>
+</head>
+<body>
+<form action="process_form_ativ_1.php" method="post">
+<table>
+<tr>
+<td colspan="2"><h2>Por favor, dê o seu feedback</h2></td>
+</tr>
+<tr>
+<td><p><strong>Nome:</strong></p></td>
+<td><input type="text" name="Nome" size="40" maxlength="40" /></td>
+</tr>
+<tr>
+<td><p><strong>Email:</strong></p></td>
+<td><input type="text" name="Email" size="40"/></td>
+</tr>
+<tr>
+<td><p><strong>Escola:</strong></p></td>
+<td><input type="text" name="Escola" size="40"/></td>
+</tr>
+<tr>
+<td><p><strong>Função:</strong></p></td>
+<td><input type="text" name="Funcao" size="40"/></td>
+</tr>
+<tr>
+<td><p><strong>Nome da Equipe:</strong></p></td>
+<td><input type="text" name="Nome_Equipe" size="40"/></td>
+</tr>
+<tr>
+<td></td>
+<td><input type="submit" name="submit" value="Enviar" /></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
