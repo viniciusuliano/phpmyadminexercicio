d1c9554abd10899fc4a477ff5afc520733e73058
Vinicius Uliano <48574234+viniciusuliano@users.noreply.github.com>
2019-08-20 10:36:27 -0300

    Create phpmyadmin.feedback2.php

---
+++ phpmyadmin.feedback2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Formulário de Feedback</title>
+</head>
+<body>
+<form action="process_form_ativ_2.php" method="post">
+<table>
+<tr>
+<td colspan="2"><h2>Por favor, dê o seu feedback</h2></td>
+</tr>
+<tr>
+<td><p><strong>Test ID:</strong></p></td>
+<td><input type="text" name="testID" size="40"/></td>
+</tr>
+<tr>
+<td><p><strong>Nome:</strong></p></td>
+<td><input type="text" name="nome" size="40" size="40" /></td>
+</tr>
+<tr>
+<td><p><strong>Idade:</strong></p></td>
+<td><input type="text" name="idade" size="40"/></td>
+</tr>
+
+<tr>
+<td></td>
+<td><input type="submit" name="submit" value="Enviar" /></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
