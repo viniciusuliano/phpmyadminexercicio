0fde0ef07def5c7f42faffd10a6b6872bcbe307a
Vinicius Uliano <48574234+viniciusuliano@users.noreply.github.com>
2019-08-20 10:37:03 -0300

    Create phpmyadmin.feedback3.php

---
+++ phpmyadmin.feedback3.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Formulário de Feedback</title>
+</head>
+<body>
+<form action="process_form_ativ_3.php" method="post">
+<table>
+<tr>
+<td colspan="2"><h2>Por favor, dê o seu feedback</h2></td>
+</tr>
+<tr>
+<td><p><strong>Fabricante:</strong></p></td>
+<td><input type="text" name="fabricante" size="40" size="40" /></td>
+</tr>
+<tr>
+<td><p><strong>Ano de Fabricação:</strong></p></td>
+<td><input type="text" name="ano_fabricacao" size="40"/></td>
+</tr>
+<tr>
+<td><p><strong>Quilometragem:</strong></p></td>
+<td><input type="text" name="quilometragem" size="40"/></td>
+</tr>
+<tr>
+<td></td>
+<td><input type="submit" name="submit" value="Enviar" /></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
